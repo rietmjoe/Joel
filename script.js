1f0b92a46b8968c9f2e10c49e3baa2844e39bfe2
rietmjoe <joelrietmann@bluewin.ch>
2026-02-04 15:39:01 +0100

    imagechange

--- script.js
+++ script.js
@@ -2,10 +2,9 @@ const person = document.getElementById("person");
 
 person.addEventListener("click", () => {
   person.classList.add("hit");
-  person.textContent = "😵";
 
   setTimeout(() => {
     person.classList.remove("hit");
-    person.textContent = "😐";
   }, 150);
 });
+
