056baa923e77c440fb3566d4fbc2c7706ea20f34
rietmjoe <joelrietmann@bluewin.ch>
2026-02-04 15:44:52 +0100

    imageafter

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 const person = document.getElementById("person");
+const img = document.getElementById("person-img");
+
+const BEFORE_SRC = "imagebefore.png";
+const AFTER_SRC = "imageafter.png";
+
+let timeoutId = null;
 
 person.addEventListener("click", () => {
+  // falls man schnell mehrfach klickt: Timer zurücksetzen
+  if (timeoutId) clearTimeout(timeoutId);
+
   person.classList.add("hit");
+  img.src = AFTER_SRC;
 
-  setTimeout(() => {
+  timeoutId = setTimeout(() => {
     person.classList.remove("hit");
-  }, 150);
+    img.src = BEFORE_SRC;
+    timeoutId = null;
+  }, 500); // 0.5 Sekunden
 });
-
