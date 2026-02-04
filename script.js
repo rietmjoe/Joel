41a35d0f4ef802d3803c516a3a916f6e81f1112e
rietmjoe <joelrietmann@bluewin.ch>
2026-02-04 15:54:46 +0100

    publish

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+console.log("script.js loaded"); // <- zum Testen
+
 const person = document.getElementById("person");
 const img = document.getElementById("person-img");
 
@@ -7,14 +9,18 @@ const AFTER_SRC = "imageafter.png";
 let timeoutId = null;
 
 person.addEventListener("click", () => {
+  console.log("clicked");
+
   if (timeoutId) clearTimeout(timeoutId);
 
   person.classList.add("hit");
-  img.src = AFTER_SRC;
+
+  // Cache umgehen (GitHub Pages cached manchmal hart)
+  img.src = AFTER_SRC + "?v=" + Date.now();
 
   timeoutId = setTimeout(() => {
     person.classList.remove("hit");
-    img.src = BEFORE_SRC;
+    img.src = BEFORE_SRC + "?v=" + Date.now();
     timeoutId = null;
   }, 500);
 });
